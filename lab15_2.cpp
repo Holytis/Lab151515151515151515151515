a92a19496d6bb84d341b368559be7424449e55dd
Holytis <holytis123@gmail.com>
2023-01-17 17:12:43 +0700

    sed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,43 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double m[][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		cout << "Row " << i+1 << ": " ;
+		for(int j=0;j<N;j++)
+		{
+			cin >> m[i][j];
+		}
+	}
+}
+
+void showMatrix(const bool m[][N])
+{
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			cout << m[i][j] << " ";
+		}
+		cout << endl ;
+	}
+}
+
+void findLocalMax(const double m[][N], bool n[][N])
+{
+	for(int i = 0 ; i < N ; i++)
+	{
+		for(int j = 0 ; j < N ; j++)
+		{
+			if ((m[i+1][j+1] >= m[i][j+1]) && (m[i+1][j+1] >= m[i+1][j]) && (m[i+1][j+1] >= m[i+1][j+2]) && (m[i+1][j+1] >= m[i+2][j+1]))
+			{
+				n[i+1][j+1] = 1 ;
+			}
+			else n[i][j] = 0 ;
+		}
+		
+	}
+}
